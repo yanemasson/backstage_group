74d3b374ef5dc72134afa66ee125755d8826cf49
yanemasson <gluedboy@gmail.com>
2025-01-11 11:55:05 +0700

    Hello World

--- src/sections/Photos/components/Photo.tsx
+++
@@ -1,9 +0,0 @@
-import {ImgHTMLAttributes} from 'react';
-
-const Photo = ({src}: ImgHTMLAttributes<HTMLImageElement>) => {
-  return (
-    <img className='w-[90vw] lg:w-[31vw] rounded-xl' alt={src} src={src}/>
-  );
-};
-
-export default Photo;
